a3e58b723b4ac9560c5e9b845f834c76775b3366
Gui Fu <szfugui@gmail.com>
2018-02-04 17:02:45 -0500

    Create ubuntu-env.sh

---
+++ ubuntu-env.sh
@@ -0,0 +1,6 @@
+apt install software-properties-common
+add-apt-repository cloud-archive:pike
+apt update 
+apt dist-upgrade
+apt install python-openstackclient
+apt install mariadb-server python-pymysql
