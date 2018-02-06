e52845fcdcd880216cf417475cc3f6f11f38dc87
Gui Fu <szfugui@gmail.com>
2018-02-05 19:30:24 -0500

    Update ntp-controller.sh

--- ntp-controller.sh
+++ ntp-controller.sh
@@ -1,2 +1,7 @@
 apt install -y chrony
 
+
+sed -i -e "s/pool 2.debian.pool.ntp.org offline iburst/server szxntp01-in.huawei.com iburst\nserver szxntp02-in.huawei.com iburst/g"  /etc/chrony/chrony.conf
+sed -i -e "67 a allow 0.0.0.0/0" /etc/chrony/chrony.conf
+
+service chrony restart
