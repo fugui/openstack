909b2593e092b8e06374d18d7fe5762a3de7250b
Gui Fu <szfugui@gmail.com>
2018-02-09 12:03:42 -0500

    Update createvm-node.sh

--- createvm-node.sh
+++ createvm-node.sh
@@ -1,16 +1,23 @@
-#/bin/sh
+#!/bin/sh
 
 INSTANCE_NAME=$1
 VOLUME_ID=`cinder create --display-name ${INSTANCE_NAME} 10 | grep -sw id | awk '{ print $4 }'`
-HOSTNAME=`cinder show $VOLUME_ID | grep "os-vol-host-attr:host" | awk '{print $4}'`
+HOSTNAME_LVM=`cinder show $VOLUME_ID | grep "os-vol-host-attr:host" | awk '{print $4}'`
 
-echo $VOLUME_ID  at $HOSTNAME
+echo $VOLUME_ID  at $HOSTNAME_LVM
 
-openstack server create --flavor CASCCE   \
-     --image a37419e2-b8b6-4adc-bdfc-e057dd4fa4fd \
-     --block-device source=$VOLUME_ID \
-     --key-name cas \
-     --availability-zone nova:dsde06 \
-     --nic net-id=provider \
-     --security-group 1aa5bf7c-5b37-430e-867e-9bbf777fd8ba \
-     ${INSTANCE_NAME}
+IFS='@'
+read -ra ADDR <<< "$HOSTNAME_LVM"
+
+if [ ${#ADDR[@]} -eq 2 ]; then
+    HOSTNAME=${ADDR[0]}
+
+    openstack server create --flavor CASCCE   \
+         --image a37419e2-b8b6-4adc-bdfc-e057dd4fa4fd \
+         --block-device source=$VOLUME_ID \
+         --key-name cas \
+         --availability-zone nova:dsde06 \
+         --nic net-id=provider \
+         --security-group 1aa5bf7c-5b37-430e-867e-9bbf777fd8ba \
+         ${INSTANCE_NAME}
+fi
