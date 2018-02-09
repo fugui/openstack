8af007146e987e0398a4850a46a4ef6143db8ec8
Gui Fu <szfugui@gmail.com>
2018-02-09 11:59:42 -0500

    Update createvm-node.sh

--- createvm-node.sh
+++ createvm-node.sh
@@ -1,14 +1,16 @@
 #/bin/sh
 
-INSTANCE_NAME=CAS2_CONSOLE=$1
+INSTANCE_NAME=$1
+VOLUME_ID=`cinder create --display-name ${INSTANCE_NAME} 10 | grep -sw id | awk '{ print $4 }'`
+HOSTNAME=`cinder show $VOLUME_ID | grep "os-vol-host-attr:host" | awk '{print $4}'`
 
-cinder create --display-name ${INSTANCE_NAME} 50
-cinder list
-openstack server create --flavor CAS16G   \
-     --image 5a2d60da-04f1-4890-b049-c2e40c946551 \
-     --block-device source=6dbaeb51-82ac-4f23-ac92-2f7ee70cf594 \
+echo $VOLUME_ID  at $HOSTNAME
+
+openstack server create --flavor CASCCE   \
+     --image a37419e2-b8b6-4adc-bdfc-e057dd4fa4fd \
+     --block-device source=$VOLUME_ID \
      --key-name cas \
-     --availability-zone nova:dsde05 \
+     --availability-zone nova:dsde06 \
      --nic net-id=provider \
      --security-group 1aa5bf7c-5b37-430e-867e-9bbf777fd8ba \
      ${INSTANCE_NAME}
