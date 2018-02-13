f316f8084cae5c2d1e5212504cd2a9c06770a53e
Gui Fu <szfugui@gmail.com>
2018-02-13 09:24:15 -0500

    Update 01-install-controller.sh

--- 01-install-controller.sh
+++ 01-install-controller.sh
@@ -29,34 +29,28 @@ echo "00 Setup the local environment...."
 apt-get update
 apt-get -y upgrade
 
-source ./00-ntp-controller.sh
+source ./controller/00-ntp-controller.sh
 
-source ./01-ubuntu-env.sh
+source ./controller/01-ubuntu-env.sh
 
-source ./03-keystone.sh
+source ./controller/03-keystone.sh
 
-source ../admin-openrc
+source ./admin-openrc
 
-source ./04-glance.sh
+source ./controller/04-glance.sh
 
-source ./05-nova.sh
+source ./controller/05-nova.sh
 
 source ./controller/051-nove-add-node.sh
 
-source ./06-neutron-self.sh
+source ./controller/06-neutron-self.sh
 
-source ./07-horizon.sh
+source ./controller/07-horizon.sh
 
 echo "Verify the installation...."
-
 openstack service list
-
 openstack image list
-
 openstack catalog list
-
 openstack compute service list
-
 nova-status upgrade check
-
 openstack network agent list
