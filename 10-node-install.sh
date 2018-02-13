05b377481f247c7779138dd316dd2d914fc7ccd7
Gui Fu <szfugui@gmail.com>
2018-02-13 15:19:04 -0500

    Update 10-node-install.sh

--- 10-node-install.sh
+++ 10-node-install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+source ./00-env.sh
+
+source ./node/00-ntp.sh
+
+source ./node/01-prepare.sh
+
+read -p "Press [Enter] key to start nova installation..."
+source ./node/02-nova-client.sh
+
+read -p "Press [Enter] key to start neutron installation..."
+source ./node/03-neutron-self.sh
+
+echo "Completed, enjoy it."
