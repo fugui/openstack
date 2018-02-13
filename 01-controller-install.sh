a31df8af8398cb1bf5a824a5b2b1a154cd51e2df
Gui Fu <szfugui@gmail.com>
2018-02-13 11:41:26 -0500

    Update 01-controller-install.sh

--- 01-controller-install.sh
+++ 01-controller-install.sh
@@ -11,18 +11,22 @@ source ./controller/00-ntp-controller.sh
 
 source ./controller/01-ubuntu-env.sh
 
+read -p "Press [Enter] key to start keystone installation..."
 source ./controller/03-keystone.sh
 
 source ./admin-openrc
 
+read -p "Press [Enter] key to start glance installation..."
 source ./controller/04-glance.sh
 
+read -p "Press [Enter] key to start nova installation..."
 source ./controller/05-nova.sh
-
 source ./controller/051-nove-add-node.sh
 
+read -p "Press [Enter] key to start neutron installation..."
 source ./controller/06-neutron-self.sh
 
+read -p "Press [Enter] key to start horizon installation..."
 source ./controller/07-horizon.sh
 
 echo "Verify the installation...."
