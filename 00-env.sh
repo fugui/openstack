16262bfee84e6bba516295a11ac80c3b8be13c15
Gui Fu <szfugui@gmail.com>
2018-02-13 13:52:28 -0500

    Update 00-env.sh

--- 00-env.sh
+++ 00-env.sh
@@ -32,11 +32,13 @@ fi
 
 NETWORK_OK=`grep ${CONTROLLER_NETWORK} /etc/network/interfaces`
 echo $NETWORK_OK
-if [ -n "$NETWORK_OK" ]; then
+if [ -z "$NETWORK_OK" ]; then
 cat >> /etc/network/interfaces <<EOF
+
 auto ${CONTROLLER_NETWORK}
 iface ${CONTROLLER_NETWORK} inet manual
   up ip link set dev $$IFACE up
   down ip link set dev $$IFACE down
+  
 EOF
 fi
